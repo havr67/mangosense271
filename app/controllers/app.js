fa9d7af2e3fbd2f5392ffa5d423b84ff79fbf75b
Nikita Khavronin <khavrks@gmail.com>
2022-02-09 15:22:32 -0500

    commit

--- app/controllers/app.js
+++ app/controllers/app.js
@@ -1,1 +1,10 @@
-const express = require("express")
+const express = require('express')
+const app = express()
+
+console.log("app started");
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
