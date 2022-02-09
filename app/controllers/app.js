c91cd7e1445403ad0a80548bcc921c258b1815b0
Nikita Khavronin <khavrks@gmail.com>
2022-02-09 18:27:55 -0500

    test

--- app/controllers/app.js
+++ app/controllers/app.js
@@ -4,6 +4,7 @@ const app = express()
 console.log("app started");
 
 const test_app = require('./test')
+const test1_app = require('./test1')
 
 //CORS set up 
 app.use(function(req, res, next) {
@@ -19,6 +20,7 @@ app.use(express.json());
 }));
 
 app.use('/', test_app)
+app.use('/hello', test1_app)
 
 // here we have error hadaling 
 app.use((req, res, next) => {
