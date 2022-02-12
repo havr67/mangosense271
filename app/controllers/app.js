dafb6c1a97a713e05db707dad541226f612fa8a1
DongYun Kim <pander0616@hotmail.com>
2022-02-11 18:19:14 -0600

    dk testing

--- app/controllers/app.js
+++ app/controllers/app.js
@@ -6,6 +6,7 @@ console.log("app started"); //Nice
 const test_app = require('./test')
 const test1_app = require('./test1')
 const test2_app = require('./test2')
+const dk_test = require('./dk')
 
 //CORS set up 
 app.use(function(req, res, next) {
@@ -23,6 +24,7 @@ app.use(express.json());
 app.use('/', test_app)
 app.use('/hello', test1_app) 
 app.use('/g', test2_app)
+app.use('/testing', dk_test)
 
 // here we have error hadaling 
 app.use((req, res, next) => {
