7dad8cf2e48e75830711a84c9a5ea62295e83abc
DongYun Kim <pander0616@hotmail.com>
2022-02-16 17:55:02 -0600

    dk testing

--- app/controllers/dk.js
+++ app/controllers/dk.js
@@ -3,18 +3,38 @@ const url = require('url')
 const routs = express.Router()
 
 
+const msg = (message) => {
+    let f = ""
+    if(message === "Testing"){
+        f = "success"
+    } else {
+        f = "fail"
+    }
+    return f
+}
+
+
 routs.get('/', (req, res, next) => {
-    console.log("in / directory")
     try {
+        let urlRequest = url.parse(req.url, true)
+        console.log(urlRequest);
+     
+        const user = urlRequest.query.user
+        const user_name = urlRequest.query.user_name
+        const user_email = urlRequest.query.user_email
+        const message = urlRequest.query.message
+
+        let g = msg(message)
         res.status(200).json({
             success: true, 
-            message: "in test1.js file, in /hello"
+            user: user, 
+            user_name: user_name,
+            user_email: user_email, 
+            message: g
         })
     } catch (e) {
-        res.status(500).json({message: "something went wrong", e})
+        res.status(500).json({message: "error", e})
     }
-
-});
 
 
 
