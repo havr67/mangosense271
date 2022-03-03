90cb836540afeb71078259893f063b1f7cb15e31
Nikita Khavronin <khavrks@gmail.com>
2022-03-02 23:06:06 -0500

    k

--- my-app/pages/chat.js
+++ my-app/pages/chat.js
@@ -1,5 +1,6 @@
-import styles from "../styles/Chat.module.scss"
 import 'bootstrap/dist/css/bootstrap.css'
+import '../styles/chat.module.css'
+import stylessass from "../styles/Chat.module.scss"
 import Image from 'next/image'
 import phota1 from '../src/images/ava1-bg.webp'
 import phota2 from '../src/images/ava2-bg.webp'
@@ -12,16 +13,16 @@ export default function Chat (){
 
 
     return(
-        <div>
-            <button class="chatbox-open"> <i class="fa fa-comment fa-2x" aria-hidden="true"></i> </button>
-            <button class="chatbox-close"> <i class="fa fa-close fa-2x" aria-hidden="true"></i> </button>
-            <section class="chatbox-popup">
-                <header class="chatbox-popup__header">
-                <aside style="flex:3"> <i class="fa fa-user-circle fa-4x chatbox-popup__avatar" aria-hidden="true"></i> </aside>
-		        <aside style="flex:8">
+        <div className="cechatscontainer">
+            <button className="chatbox-open"> <i className="fa fa-comment fa-2x" aria-hidden="true"></i> </button>
+            <button className="chatbox-close"> <i className="fa fa-close fa-2x" aria-hidden="true"></i> </button>
+            <section className="chatbox-popup">
+                <header className="chatbox-popup__header">
+                <aside className="flex3"> <i className="fa fa-user-circle fa-4x chatbox-popup__avatar" aria-hidden="true"></i> </aside>
+		        <aside className="flex8">
 			        <h1>Sussanah Austin</h1> Agent (Online) </aside>
-		        <aside style="flex:1">
-			        <button class="chatbox-maximize"><i class="fa fa-window-maximize" aria-hidden="true"></i></button>
+		        <aside className="flex1">
+			        <button className="chatbox-maximize"><i className="fa fa-window-maximize" aria-hidden="true"></i></button>
 		        </aside>
                 </header>
             </section>
